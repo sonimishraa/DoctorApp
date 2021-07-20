61f06d30dd2af742545062f548832b191393787f
DESKTOP-53KE5M4\DELL <mishrasoni.iitr@gmail.com>
2021-07-20 13:37:17 +0530

    line bt toolbar & tablayout issue

--- app/src/main/java/com/iotric/doctorplus/activity/DrDashboardActivity.kt
+++ app/src/main/java/com/iotric/doctorplus/activity/DrDashboardActivity.kt
@@ -1,7 +1,6 @@
 package com.iotric.doctorplus.activity
 
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -10,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.navigation.NavigationView
 import com.iotric.doctorplus.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,17 +21,26 @@ class DrDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dr_activity_dashbord)
+        initView()
+    }
+
+    private fun initView() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        toolbar.setTitleTextColor(getColor(R.color.white))
-        setSupportActionBar(toolbar)
-        toolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24)
-        toolbar.navigationIcon ?.setTint(getColor(R.color.white))
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer)
         val navView: NavigationView = findViewById(R.id.nav)
         val navController = findNavController(R.id.nav_host)
+        val appbar = findViewById<AppBarLayout>(R.id.appbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24)
+        toolbar.navigationIcon?.setTint(getColor(R.color.white))
+        appbar.setOutlineProvider(null)
+        setSupportActionBar(toolbar)
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_dashboard, R.id.navigation_profile, R.id.navigation_logout
+                R.id.navigation_dashboard, R.id.navigation_profile, R.id.navigation_logout,
+                R.id.navigation_privacy, R.id.navigation_faq, R.id.navigation_terms,
+                R.id.navigation_logout, R.id.navigation_notifications, R.id.navigation_lab
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
